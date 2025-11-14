2c6830cd69d1f9292c5f041bdd3aba0a9c3b22e9
jakeerhussainforgit <100121149+jakeerhussainforgit@users.noreply.github.com>
2025-11-15 00:14:23 +0530

    dev2 added code to appp.java

--- app.java
+++ app.java
@@ -1,1 +1,16 @@
 jabavajbsabcas c cmn mXXCAKNC CA A 
+
+//code adding by dev2
+public class AsciiValue {
+
+    public static void main(String[] args) {
+
+        char ch = 'a';
+        int ascii = ch;
+        // You can also cast char to int
+        int castAscii = (int) ch;
+
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+        System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+    }
+}
